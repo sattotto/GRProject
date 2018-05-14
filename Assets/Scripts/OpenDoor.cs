35bf17019a9d215d4ffb12a8898a8c4f2f2e99a1
佐藤秀輔 <sato.fun0619@gmail.com>
2018-05-14 14:53:43 +0900

    DoorObjectの追加

---
+++ Assets/Scripts/OpenDoor.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenDoor : MonoBehaviour {
+
+	private bool isNear;
+	private Animator animator;
+	// Use this for initialization
+	void Start () {
+		isNear = false;
+		animator = GameObject.Find("doorObject/Door").GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown("space") && isNear) {
+			animator.SetBool ("isNear", !animator.GetBool ("isNear"));
+		}
+	}
+ 
+	void OnTriggerEnter(Collider col) {
+		if(col.tag == "Player") {
+			isNear = true;
+		}
+	}
+ 
+	void OnTriggerExit(Collider col) {
+		animator.SetBool ("isNear", false);
+	}
+}
