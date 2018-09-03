a8f42fc0ff4cb027bb5f65b54fbb643c1732cce7
satoto <sato.fun0619@gmail.com>
2018-09-03 15:47:51 +0900

    update charactor vision method

--- Assets/Scripts/VRCharactorMoveController.cs
+++ Assets/Scripts/VRCharactorMoveController.cs
@@ -3,32 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class VRCharactorMoveController : MonoBehaviour {
-    /*
-    public float speed = 3.0f;
 
-    CharacterController controller;
-
-    void Start()
-    {
-        controller = GetComponent<CharacterController>();
-    }
-
-    void Update()
-    {
-
-        Debug.Log(Mathf.Sin(this.transform.rotation.y * Mathf.Deg2Rad) + "  :  " + Mathf.Cos(this.transform.rotation.y * Mathf.Deg2Rad));
-        // 左手のアナログスティックの向きを取得
-        Vector2 stickL = OVRInput.Get(OVRInput.RawAxis2D.LThumbstick);
-        Vector3 direction = new Vector3(-stickL.y*speed, 0, stickL.x*speed);
-
-        controller.SimpleMove(direction);
-    }
-    */
     public float speed = 3.0F;
     public float jumpSpeed = 8.0F;
     public float gravity = 20.0F;
     private Vector3 moveDirection = Vector3.zero;
+
+    private const float ANGLE_LIMIT_UP = 60f;
+    private const float ANGLE_LIMIT_DOWN = -60f;
+
     void Update()
+    {
+        MoveController();
+        VisionController();
+    }
+
+    private void MoveController()
     {
         CharacterController controller = GetComponent<CharacterController>();
         if (controller.isGrounded)
@@ -43,5 +33,20 @@ public class VRCharactorMoveController : MonoBehaviour {
         }
         moveDirection.y -= gravity * Time.deltaTime;
         controller.Move(moveDirection * Time.deltaTime);
+    }
+
+    private void VisionController()
+    {
+        // 右手のアナログスティックの向きを取得
+        Vector2 stickR = OVRInput.Get(OVRInput.RawAxis2D.RThumbstick);
+        this.transform.eulerAngles += new Vector3(0, stickR.x, stickR.y);
+
+        float angle_z = 180f <= transform.eulerAngles.z ? transform.eulerAngles.z - 360 : transform.eulerAngles.z;
+        Debug.Log(angle_z);
+        transform.eulerAngles = new Vector3(
+            0,
+            transform.eulerAngles.y,
+            Mathf.Clamp(angle_z, ANGLE_LIMIT_DOWN, ANGLE_LIMIT_UP)
+        );
     }
 }
