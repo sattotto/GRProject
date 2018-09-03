8d7a399da7f4903daa91e4b7cfff762d3166f1d6
satoto <sato.fun0619@gmail.com>
2018-09-03 14:31:04 +0900

    update Charactor moving method

---
+++ Assets/Scripts/GrabController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrabController : MonoBehaviour {
+
+    bool itemGrabFlg = false;
+
+	void OnTriggerEnter(Collider other) {
+		if (other.gameObject.tag == "item0") {
+			Debug.Log("hoge");
+            itemGrabFlg = true;
+		}
+	}
+
+	void OnTriggerExit(Collider other){
+        if (other.gameObject.tag == "item0") {
+            Debug.Log("fuga");
+            itemGrabFlg = false;
+        }
+    }
+
+	void Update () {
+
+        if (OVRInput.GetDown(OVRInput.RawButton.RHandTrigger))
+        {
+            // プレハブを取得
+            GameObject prefab = (GameObject)Resources.Load("Prefabs/ToyCubePf");
+            // プレハブからインスタンスを生成
+            Instantiate(prefab, new Vector3(1, 1, 1), Quaternion.identity);
+        } else
+        {
+            
+        }
+    }
+
+}
---
+++ Assets/Scripts/VRCharactorMoveController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VRCharactorMoveController : MonoBehaviour {
+    /*
+    public float speed = 3.0f;
+
+    CharacterController controller;
+
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+
+        Debug.Log(Mathf.Sin(this.transform.rotation.y * Mathf.Deg2Rad) + "  :  " + Mathf.Cos(this.transform.rotation.y * Mathf.Deg2Rad));
+        // 左手のアナログスティックの向きを取得
+        Vector2 stickL = OVRInput.Get(OVRInput.RawAxis2D.LThumbstick);
+        Vector3 direction = new Vector3(-stickL.y*speed, 0, stickL.x*speed);
+
+        controller.SimpleMove(direction);
+    }
+    */
+    public float speed = 3.0F;
+    public float jumpSpeed = 8.0F;
+    public float gravity = 20.0F;
+    private Vector3 moveDirection = Vector3.zero;
+    void Update()
+    {
+        CharacterController controller = GetComponent<CharacterController>();
+        if (controller.isGrounded)
+        {
+            Vector2 stickL = OVRInput.Get(OVRInput.RawAxis2D.LThumbstick);
+            moveDirection = new Vector3(-stickL.y * speed, 0, stickL.x * speed);
+            moveDirection = transform.TransformDirection(moveDirection);
+            moveDirection *= speed;
+            if (Input.GetButton("Jump"))
+                moveDirection.y = jumpSpeed;
+
+        }
+        moveDirection.y -= gravity * Time.deltaTime;
+        controller.Move(moveDirection * Time.deltaTime);
+    }
+}
