e4536190beeb8e3f6594df83eab149377a7984c8
satoto <sato.fun0619@gmail.com>
2018-10-23 17:52:52 +0900

    感情取得のデバック追加

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            int[] hoge = new int[2];
+            hoge = PlayerEmotions.getMaxEmotion();
+            Debug.Log("EmoNum : " + hoge[0] +", EmoValue : " + hoge[1]);
+        }
+    }
+}
