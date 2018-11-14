00993eabf4941b0e9484c7b8f840d87ee1452fcf
satoto <sato.fun0619@gmail.com>
2018-11-14 14:14:50 +0900

    左右のGrabberで挙動を変える必要がなかったので統一

---
+++ Assets/Scripts/GrabScript/GrabberTrigerController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrabberTrigerController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    void OnTriggerEnter(Collider other) {
+        if (other.gameObject.tag == "item0")
+        {
+            Debug.Log("in " + other.gameObject.tag);
+            GrabController.itemGrabFlg = true;
+        }
+        
+    }
+
+    void OnTriggerStay(Collider other)
+    {
+        Debug.Log("Hand stay " + other.gameObject.tag);
+        if (other.gameObject.tag == "targetObj-1")
+        {
+            GrabController.objItemGrabFlg = true;
+            GrabController.objectName = other.gameObject.name;
+        }
+    }
+
+    void OnTriggerExit(Collider other) {
+        if (other.gameObject.tag == "targetObj-1")
+        {
+            GrabController.objItemGrabFlg = false;
+        }
+		if (other.gameObject.tag == "item0" && (OVRInput.GetDown(OVRInput.RawButton.RHandTrigger) || OVRInput.GetDown(OVRInput.RawButton.LHandTrigger))) {
+            GrabController.itemGrabFlg = false;
+        }
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+	}
+}
