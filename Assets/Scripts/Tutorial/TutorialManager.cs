1a913a53a7e10dbf0f23808c2e7009ac4b2897ac
satoto <sato.fun0619@gmail.com>
2018-12-10 19:35:37 +0900

    Tutorial時のメッセージ送りの変更

--- Assets/Scripts/Tutorial/TutorialManager.cs
+++ Assets/Scripts/Tutorial/TutorialManager.cs
@@ -7,18 +7,25 @@ public class TutorialManager : MonoBehaviour {
 
 	public GameObject VRCharacter;
 
+	[SerializeField]
+	private MessageScript msgScript;
+
+	private float duration;
+
 	// Use this for initialization
 	void Start () {
-		
+		msgScript.SetMessagePanel("this is tutorial");
+		duration = 0;
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		duration += Time.deltaTime;
 		nextStage();
 	}
 
 	private void nextStage() {
-		if(VRCharacter.transform.position.y < -1) {
+		if(VRCharacter.transform.position.y < -1 || Input.GetKeyDown(KeyCode.Return) || duration > 300) {
 			SceneManager.LoadScene("Main");
 		}
 	}
